ef3c44c483709f6418834cf31cc9541b4a2af57c
TacticalCoders <docdoca@naver.com>
2022-08-04 17:19:31 +0900

    스크랩 결과 딕셔너리로 반환

--- brunch_post_scraper.py
+++ brunch_post_scraper.py
@@ -40,54 +40,64 @@ def no_comma(str):
     text = str.replace(',','')
     return text
 
+def scrap_single_post(url):
+    html = download(url)
+    soup = BeautifulSoup(html, 'lxml')
+
+    title = soup.find('h1',attrs={'class':'cover_title'}).text
+
+    sub_title = soup.find('p', attrs={'class':'cover_sub_title'}).text
+
+    body_text = soup.find_all(['p','h2','h3','h4','h5','h6'], attrs={'class':'wrap_item item_type_text'})
+    text = ''
+    for s in body_text:
+        text = text + ' ' + s.text.replace('\xa0',' ')
+
+    likes = soup.find('span', attrs={'class': 'f_l text_like_count text_default text_with_img_ico ico_likeit_like #like'}).text
+    likes = 0 if likes=='' else int(no_comma(likes))
+
+    num_comments = soup.find('span', attrs={'class':'f_l text_comment_count text_default text_with_img_ico'}).text
+    num_comments = 0 if num_comments=='' else int(no_comma(num_comments))
+
+    post_date = soup.find('span',attrs={'class':'f_l date'}).text
+
+    keyword_list = []
+    first_keyword = soup.find('ul', attrs={'class':'list_keyword'}).li
+    keyword_list.append(no_spaces(first_keyword.get_text()))
+    other_keywords = first_keyword.find_next_siblings('li')
+    for keyword in other_keywords:
+        keyword_list.append(no_spaces(keyword.text))
+
+    author = soup.find('span', attrs={'class':'f_l text_author #author'}).a.text
+    author_page = soup.find('span', attrs={'class':'f_l text_author #author'}).a['href']
+    author_id = author_page.replace('https://brunch.co.kr/','')
+    try:
+        author_belong = soup.find('span', attrs={'class':'author_belong'}).span.find_next_sibling('span').get_text()
+    except AttributeError as e:
+        author_belong = "직업 없음"
+    author_desc = soup.find('p', attrs={'class':'txt_desc'}).text
+    num_subscription = soup.find('span', attrs={'class':'num_subscription'}).text
+    num_subscription = 0 if num_subscription=='' else int(no_comma(num_subscription))
+
+    print('title:', title)
+    print('sub_title:', sub_title)
+    print('body_text:', text)
+    print('keywords:', keyword_list)
+    print('likes:',likes)
+    print('num_comments:', num_comments)
+    print('post_date:', decode_post_date(post_date))
+    print('author:', author)
+    print('author_id:', author_id)
+    print('author_belong:',author_belong)
+    print('author_desc:', author_desc)
+    print('num_subscription:', num_subscription)
+
+    scrap_result = {'title':title, 'sub_title':sub_title, 'body_text':text, 'keywords':keyword_list, 'likes':likes, 
+                    'num_comments':num_comments,'post_date':post_date, 'post_url':url, 'author':author, 'author_id':author_id,
+                    'author_belong':author_belong, 'author_desc':author_desc, 'num_subscription':num_subscription }
+
+    return scrap_result                
+
 url = input('스크랩하고자 하는 url을 입력해주세요: ')
-html = download(url)
-soup = BeautifulSoup(html, 'lxml')
-
-title = soup.find('h1',attrs={'class':'cover_title'}).text
-
-sub_title = soup.find('p', attrs={'class':'cover_sub_title'}).text
-
-body_text = soup.find_all(['p','h4'], attrs={'class':'wrap_item item_type_text'})
-text = ''
-for s in body_text:
-    text = text + ' ' + s.text.replace('\xa0',' ')
-
-likes = soup.find('span', attrs={'class': 'f_l text_like_count text_default text_with_img_ico ico_likeit_like #like'}).text
-likes = 0 if likes=='' else int(no_comma(likes))
-
-num_comments = soup.find('span', attrs={'class':'f_l text_comment_count text_default text_with_img_ico'}).text
-num_comments = 0 if num_comments=='' else int(no_comma(num_comments))
-
-post_date = soup.find('span',attrs={'class':'f_l date'}).text
-
-keyword_list = []
-first_keyword = soup.find('ul', attrs={'class':'list_keyword'}).li
-keyword_list.append(no_spaces(first_keyword.get_text()))
-other_keywords = first_keyword.find_next_siblings('li')
-for keyword in other_keywords:
-    keyword_list.append(no_spaces(keyword.text))
-
-author = soup.find('span', attrs={'class':'f_l text_author #author'}).a.text
-author_page = soup.find('span', attrs={'class':'f_l text_author #author'}).a['href']
-author_id = author_page.replace('https://brunch.co.kr/','')
-try:
-    author_belong = soup.find('span', attrs={'class':'author_belong'}).span.find_next_sibling('span').get_text()
-except AttributeError as e:
-    author_belong = "직업없음"
-author_desc = soup.find('p', attrs={'class':'txt_desc'}).text
-num_subscription = soup.find('span', attrs={'class':'num_subscription'}).text
-num_subscription = 0 if num_subscription=='' else int(no_comma(num_subscription))
-
-print('title:', title)
-print('sub_title:', sub_title)
-print('body_text:', text)
-print('likes:',likes)
-print('num_comments:', )
-print('post_date:', decode_post_date(post_date))
-print('kewords:', keyword_list)
-print('author:', author)
-print('author_id:', author_id)
-print('author_belong:',author_belong)
-print('author_desc:', author_desc)
-print('num_subscription:', num_subscription)
+result = scrap_single_post(url)
+print(result)
